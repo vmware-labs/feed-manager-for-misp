4a48becef07b534437bd1083093c3b942d0ab67b
Stefano Ortolani <ortolanis@vmware.com>
2023-03-20 17:03:00 +0000

    Add support for sighting source

--- src/feed_manager/translator.py
+++ src/feed_manager/translator.py
@@ -1,5 +1,6 @@
 # Copyright 2022 VMware, Inc.
 # SPDX-License-Identifier: BSD-2
+import collections
 import datetime
 import itertools
 import feed_manager
@@ -219,12 +220,7 @@ class SightingUtils:
     @classmethod
     def sighting_to_timestamp(cls, sighting_object: pymisp.MISPSighting) -> int:
         """Get the timestamp of a sighting object."""
-        try:
-            # Initialized Sightings have a 'date_sighting' attribute
-            return int(sighting_object.date_sighting)
-        except AttributeError:
-            # Uninitialized Sightings have a 'timestamp' attribute
-            return int(sighting_object.timestamp)
+        return int(sighting_object.date_sighting)
 
     @classmethod
     def get_sightings(
@@ -255,22 +251,49 @@ class SightingUtils:
         date_objects: Iterable[datetime.datetime],
     ) -> List[pymisp.MISPSighting]:
         """Update sightings."""
-        sighting_objects = []
-        for date_object in date_objects:
-            sighting = pymisp.MISPSighting()
-            sighting.from_dict(**{"timestamp": date_object.timestamp()})
-            sighting_objects.append(sighting)
-        return sighting_objects
+        return [cls.create_sighting(x) for x in date_objects]
+
+    @classmethod
+    def create_sighting(
+        cls,
+        date_object: datetime.datetime,
+        sighting_type: str = "0",
+        sighting_source: Optional[str] = None,
+    ) -> pymisp.MISPSighting:
+        """Create a sighting."""
+        sighting_timestamp = date_object.timestamp()
+        kwargs = {
+            "timestamp": sighting_timestamp,  # this is read by pymisp
+            "date_sighting": sighting_timestamp,  # this is for consistency with returned sightings
+            "type": sighting_type,
+        }
+        if sighting_source:
+            kwargs["source"] = sighting_source
+        sighting = pymisp.MISPSighting()
+        sighting.from_dict(**kwargs)
+        return sighting
 
     @classmethod
     def has_sighting(
         cls,
         entity: Union[pymisp.MISPAttribute, pymisp.MISPObject],
         sighting: pymisp.MISPSighting,
+        compare_source: bool = False,
     ) -> bool:
         """Return whether the object has a sighting with this timestamp."""
-        existing_timestamps = set(cls.get_sightings_timestamp(entity))
-        return cls.sighting_to_timestamp(sighting) in existing_timestamps
+        # convert current and existing sightings into timestamps
+        sighting_ts = cls.sighting_to_timestamp(sighting)
+        sightings_by_ts = collections.defaultdict(list)
+        for x in cls.get_sightings(entity):
+            sightings_by_ts[cls.sighting_to_timestamp(x)].append(x)
+        # if it is not in the mapping, we have no match
+        if sighting_ts not in sightings_by_ts:
+            return False
+        # if we have a match, but we are not comparing the source, then we have it already
+        if not compare_source:
+            return True
+        # otherwise compare all the sources
+        return any(x.source == sighting.source for x in sightings_by_ts.get(sighting_ts, []))
 
     @classmethod
     def iter_sightings(
@@ -333,7 +356,7 @@ class IndicatorTranslator:
             if ":" in network_indicator:
                 attribute_type = "ip-dst|port"
             else:
-                attribute_type = "ip"
+                attribute_type = "ip-dst"
         else:
             attribute_type = "domain"
         net_attribute = pymisp.MISPAttribute()
